ec4440c2948bb447ae705adcb98a5c1973ddc6b9
Eric Boh <cossish@gmail.com>
2012-04-12 23:27:08 +0200

    Added UrlUtils

---
+++ src/bnetlib/UrlUtils.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category  bnetlib
+ * @package   UrlUtils
+ * @copyright 2012 Eric Boh <cossish@gmail.com>
+ * @license   https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+
+namespace bnetlib;
+
+/**
+ * @category  bnetlib
+ * @package   UrlUtils
+ * @copyright 2012 Eric Boh <cossish@gmail.com>
+ * @license   https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+class UrlUtils
+{
+    /**
+     * @var array
+     */
+    static protected $replaceRuleSets = array(
+        array(array('-', '\'', '(', ')', '/'), ''),
+        array(' ', '-'),
+        array('--', '-')
+      );
+
+    /**
+     * @var array
+     */
+    static protected $normalizeCharacter = array(
+       'À' => 'A',  'Á' => 'A',  'Â' => 'A',
+       'Ã' => 'A',  'Ä' => 'A',  'Å' => 'A',
+       'Æ' => 'A',  'Ç' => 'C',  'È' => 'E',
+       'É' => 'E',  'Ê' => 'E',  'Ë' => 'E',
+       'Ì' => 'I',  'Í' => 'I',  'Î' => 'I',
+       'Ï' => 'I',  'Ð' => 'Dj', 'Ñ' => 'N',
+       'Ò' => 'O',  'Ó' => 'O',  'Ô' => 'O',
+       'Õ' => 'O',  'Ö' => 'O',  'Ø' => 'O',
+       'Ù' => 'U',  'Ú' => 'U',  'Û' => 'U',
+       'Ü' => 'U',  'Ý' => 'Y',  'Þ' => 'B',
+       'ß' => 'Ss', 'à' => 'a',  'á' => 'a',
+       'â' => 'a',  'ã' => 'a',  'ä' => 'a',
+       'å' => 'a',  'æ' => 'a',  'ç' => 'c',
+       'è' => 'e',  'é' => 'e',  'ê' => 'e',
+       'ë' => 'e',  'ì' => 'i',  'í' => 'i',
+       'î' => 'i',  'ï' => 'i',  'ð' => 'o',
+       'ñ' => 'n',  'й' => 'и',  'ò' => 'o',
+       'ó' => 'o',  'ô' => 'o',  'õ' => 'o',
+       'ö' => 'o',  'ø' => 'o',  'ù' => 'u',
+       'ú' => 'u',  'û' => 'u',  'ý' => 'y',
+       'þ' => 'b',  'ÿ' => 'y',  'Š' => 'S',
+       'š' => 's',  'Ž' => 'Z',  'ž' => 'z',
+       'ƒ' => 'f'
+    );
+
+    /**
+     * @param  string $realm
+     * @return string
+     */
+    static public function slug($realm)
+    {
+        foreach (self::$replaceRuleSets as $rule) {
+            $realm = str_replace($rule[0], $rule[1], $realm);
+        }
+
+        if (!preg_match('/^[a-z-]+$/i', $realm)) {
+            // Normalizer::normalize Ha!
+            foreach (self::$normalizeCharacter as $old => $new) {
+                $realm = str_replace($old, $new, $realm);
+            }
+        }
+
+        return mb_strtolower($realm, 'UTF-8');
+    }
+}
---
+++ tests/bnetlibTest/UrlUtilsTest.php
@@ -0,0 +1,171 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage UnitTests
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+
+namespace bnetlibTest;
+
+use bnetlib\UrlUtils;
+
+/**
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage UnitTests
+ * @group      Configuration
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+class UrlUtilsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSlugStrippingCharacters()
+    {
+        $name = 'Simple (Te\'st / cool-hmm)';
+        $slug = 'simple-test-coolhmm';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugNormalizingSpecialCharacters()
+    {
+        $name = 'La Croisade écarlate';
+        $slug = 'la-croisade-ecarlate';
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+
+        $name = 'Marécage de Zangar';
+        $slug = 'marecage-de-zangar';
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+
+        $name = 'Aggra (Português)';
+        $slug = 'aggra-portugues';
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugSlugUsLocale()
+    {
+        $name = 'Aegwynn';
+        $slug = 'aegwynn';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugMxLocale()
+    {
+        /**
+         * As for now, 2012-04-10, no Latin American realms uses special characters,
+         * so this test is pretty much useless...
+         * @see http://us.battle.net/support/en/article/latin-america-world-of-warcraft-faq#q-3
+         */
+        $name = 'Aegwynn';
+        $slug = 'aegwynn';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugBrLocale()
+    {
+        /**
+         * Same as in testSlugMxLocale()
+         */
+        $name = 'Tol Barad';
+        $slug = 'tol-barad';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugGbLocale()
+    {
+        $name = 'Blade\'s Edge';
+        $slug = 'blades-edge';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugEsLocale()
+    {
+        /**
+         * Same as in testSlugMxLocale()
+         */
+        $name = 'Shen\'dralar';
+        $slug = 'shendralar';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugFrLocale()
+    {
+        $name = 'Chants éternels';
+        $slug = 'chants-eternels';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugRuLocale()
+    {
+        $name = 'Борейская тундра';
+        $slug = 'бореиская-тундра';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugDeLocale()
+    {
+        $name = 'Die ewige Wacht';
+        $slug = 'die-ewige-wacht';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugItLocale()
+    {
+        /**
+         * As for now, 2012-04-10, there are no Italian realms
+         */
+        $name = 'Burning Legion';
+        $slug = 'burning-legion';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugPtLocale()
+    {
+        $name = 'Aggra (Português)';
+        $slug = 'aggra-portugues';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugKrLocale()
+    {
+        $name = '쿨 티라스';
+        $slug = '쿨-티라스';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugTwLocale()
+    {
+        $name = '世界之樹';
+        $slug = '世界之樹';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+
+    public function testSlugCnLocale()
+    {
+        $name = '万色星辰';
+        $slug = '万色星辰';
+
+        $this->assertEquals(UrlUtils::slug($name), $slug);
+    }
+}
