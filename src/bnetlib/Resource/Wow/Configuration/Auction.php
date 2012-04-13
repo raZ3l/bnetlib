f048612afea83ef056ed7158cbaf87a3164410cf
Eric Boh <cossish@gmail.com>
2012-04-13 10:13:16 +0200

    Added Auction config + tests

---
+++ src/bnetlib/Resource/Wow/Configuration/Auction.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage WorldOfWarcraft
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+
+namespace bnetlib\Resource\Wow\Configuration;
+
+use bnetlib\UrlUtils;
+use bnetlib\Resource\ConfigurationInterface;
+
+/**
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage WorldOfWarcraft
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+class Auction implements ConfigurationInterface
+{
+    /**
+     * @const string
+     */
+    const RESOURCE_URL = '/api/wow/auction/data/%s';
+
+    /**
+     * @var int
+     */
+    protected $resourceType = self::TYPE_DYNAMIC_PATH;
+
+    /**
+     * @var array
+     */
+    protected $argumentAliases = array('realm' => 'slug');
+
+    /**
+     * @var array
+     */
+    protected $requiredArguments = array('realm');
+
+    /**
+     * @var null
+     */
+    protected $optionalArguments = null;
+
+    /**
+     * @var array
+     */
+    protected $manipulableArguments;
+
+    /**
+     * Setting closures.
+     */
+    public function __construct()
+    {
+        $this->manipulableArguments = array(
+            'realm' => function ($v) {
+                return UrlUtils::slug($v);
+            }
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getResourceType()
+    {
+        return $this->resourceType;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getArgumentAliases()
+    {
+        return $this->argumentAliases;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRequiredArguments()
+    {
+        return $this->requiredArguments;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getOptionalArguments()
+    {
+        return $this->optionalArguments;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getManipulableArguments()
+    {
+        return $this->manipulableArguments;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isAuthenticationPossible()
+    {
+        return true;
+    }
+}
