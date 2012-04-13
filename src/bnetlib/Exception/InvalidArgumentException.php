67371ef78c2c06099cbf8dfa01c75dcf3c84fbb7
Eric Boh <cossish@gmail.com>
2012-04-13 09:40:28 +0200

    Added Exceptions

---
+++ src/bnetlib/Exception/InvalidArgumentException.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category  bnetlib
+ * @package   Exception
+ * @copyright 2012 Eric Boh <cossish@gmail.com>
+ * @license   https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+
+namespace bnetlib\Exception;
+
+use bnetlib\Exception;
+
+/**
+ * @category  bnetlib
+ * @package   Exception
+ * @copyright 2012 Eric Boh <cossish@gmail.com>
+ * @license   https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+class InvalidArgumentException extends \InvalidArgumentException implements Exception
+{
+
+}
