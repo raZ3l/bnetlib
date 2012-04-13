598d137c3708201ecd1800abb47b805143f24781
Eric Boh <cossish@gmail.com>
2012-04-13 18:59:56 +0200

    Locale support for resources

---
+++ src/bnetlib/Locale/LocaleAwareInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category  bnetlib
+ * @package   Locale
+ * @copyright 2012 Eric Boh <cossish@gmail.com>
+ * @license   https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+
+namespace bnetlib\Locale;
+
+/**
+ * @category  bnetlib
+ * @package   Locale
+ * @copyright 2012 Eric Boh <cossish@gmail.com>
+ * @license   https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+interface LocaleAwareInterface
+{
+    /**
+     * @param $locale Locatable
+     */
+    public function setLocale(Locatable $locale);
+}
