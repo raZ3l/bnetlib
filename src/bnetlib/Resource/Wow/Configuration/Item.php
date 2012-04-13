1b40a3299e4c6edce8048031a1e9fa0c4674bd0a
Eric Boh <cossish@gmail.com>
2012-04-13 10:45:54 +0200

    Added ItemClasses config + tests

---
+++ src/bnetlib/Resource/Wow/Configuration/Item.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage WorldOfWarcraft
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+
+namespace bnetlib\Resource\Wow\Configuration;
+
+use bnetlib\Resource\ConfigurationInterface;
+
+/**
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage WorldOfWarcraft
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+class Item implements ConfigurationInterface
+{
+    /**
+     * @const string
+     */
+    const RESOURCE_URL = '/api/wow/item/%s';
+
+    /**
+     * @var int
+     */
+    protected $resourceType = self::TYPE_DYNAMIC_PATH;
+
+    /**
+     * @var null
+     */
+    protected $argumentAliases = null;
+
+    /**
+     * @var array
+     */
+    protected $requiredArguments = array('id');
+
+    /**
+     * @var null
+     */
+    protected $optionalArguments = null;
+
+    /**
+     * @var null
+     */
+    protected $manipulableArguments = null;
+
+    /**
+     * @inheritdoc
+     */
+    public function getResourceType()
+    {
+        return $this->resourceType;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getArgumentAliases()
+    {
+        return $this->argumentAliases;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRequiredArguments()
+    {
+        return $this->requiredArguments;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getOptionalArguments()
+    {
+        return $this->optionalArguments;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getManipulableArguments()
+    {
+        return $this->manipulableArguments;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isAuthenticationPossible()
+    {
+        return true;
+    }
+}
