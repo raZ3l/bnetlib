6a140adddb83062875e533e5633124b762271c49
Eric Boh <cossish@gmail.com>
2012-04-13 17:01:28 +0200

    Added Thumbnail config + tests

---
+++ src/bnetlib/Resource/Wow/Configuration/Thumbnail.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage WorldOfWarcraft
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+
+namespace bnetlib\Resource\Wow\Configuration;
+
+use bnetlib\UrlUtils;
+use bnetlib\Resource\ConfigurationInterface;
+
+/**
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage WorldOfWarcraft
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+class Thumbnail implements ConfigurationInterface
+{
+    /**
+     * @const string
+     */
+    const RESOURCE_URL = '/static-render/%s/%s';
+
+    /**
+     * @var int
+     */
+    protected $resourceType = self::TYPE_DYNAMIC_PATH;
+
+    /**
+     * @var array
+     */
+    protected $requiredArguments = array('region', 'thumbnail');
+
+    /**
+     * @inheritdoc
+     */
+    public function getResourceType()
+    {
+        return $this->resourceType;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getArgumentAliases()
+    {
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRequiredArguments()
+    {
+        return $this->requiredArguments;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getOptionalArguments()
+    {
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getManipulableArguments()
+    {
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isAuthenticationPossible()
+    {
+        return true;
+    }
+}
