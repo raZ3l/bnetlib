cf7baa3c17e60e74b9c10a459274718af0d7daab
Eric Boh <cossish@gmail.com>
2012-04-26 00:43:17 +0200

    Added tests for shared resource classes

---
+++ tests/bnetlibTest/Resource/Wow/Shared/AchievementTest.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage UnitTests
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+
+namespace bnetlibTest\Resource\Wow\Shared;
+
+use bnetlib\Resource\Wow\Shared\Achievement;
+
+/**
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage UnitTests
+ * @group      WorldOFWarcraft
+ * @group      WoW_Shared
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+class AchievementTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var bnetlib\Resource\Wow\Shared\Achievement
+     */
+    protected static $obj;
+
+    public static function setUpBeforeClass()
+    {
+        self::$obj = new Achievement();
+        self::$obj->populate(array(
+            'a'   => 6,
+            'ts'  => 1224092732000,
+            'c'   => 34,
+            'cq'  => 85,
+            'cts' => 1333377449000,
+            'cc'  => 1333377449000
+        ));
+
+    }
+
+    public static function tearDownAfterClass()
+    {
+        self::$obj = null;
+    }
+
+    public function testId()
+    {
+       $this->assertEquals(6, self::$obj->getId());
+    }
+
+    public function testTimestamp()
+    {
+        $this->assertEquals(1224092732000, self::$obj->getTimestamp());
+    }
+
+    public function testCriteria()
+    {
+        $this->assertEquals(34, self::$obj->getCriteria());
+    }
+
+    public function testCriteriaQuantity()
+    {
+        $this->assertEquals(85, self::$obj->getCriteriaQuantity());
+    }
+
+    public function testCriteriaTimestamp()
+    {
+        $this->assertEquals(1333377449000, self::$obj->getCriteriaTimestamp());
+    }
+
+    public function testCriteriaCreated()
+    {
+        $this->assertEquals(1333377449000, self::$obj->getCriteriaCreated());
+    }
+}
---
+++ tests/bnetlibTest/Resource/Wow/Shared/ListDataTest.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage UnitTests
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+
+namespace bnetlibTest\Resource\Wow\Shared;
+
+use bnetlib\Resource\Wow\Shared\ListData;
+
+/**
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage UnitTests
+ * @group      WorldOFWarcraft
+ * @group      WoW_Shared
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    https://gitbub.com/coss/bnetlib/LISENCE     MIT License
+ */
+class ListDataTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var bnetlib\Resource\Wow\Shared\ListData
+     */
+    protected static $obj;
+
+    public static function setUpBeforeClass()
+    {
+        $data            = array();
+        $data['content'] = json_decode(file_get_contents(
+            dirname(__DIR__) . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . 'character.json'
+        ), true);;
+
+        self::$obj = new ListData();
+        self::$obj->populate($data['content']['quests']);
+
+    }
+
+    public static function tearDownAfterClass()
+    {
+        self::$obj = null;
+    }
+
+    public function testCountable()
+    {
+        $this->assertEquals(5, count(self::$obj));
+    }
+
+    public function testIterator()
+    {
+        foreach (self::$obj as $key => $item) {
+            $this->assertInternalType('integer', $item);
+        }
+    }
+
+    public function testHas()
+    {
+        $this->assertTrue(self::$obj->has(13136));
+    }
+}
