3666e0a02644ee2f8fceab54a9b98651dda3b128
Eric Boh <cossish@gmail.com>
2012-04-27 17:51:39 +0200

    Added Battlegroups resource + tests

---
+++ src/bnetlib/Resource/Wow/Battlegroups/Battlegroup.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage WorldOfWarcraft
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    http://coss.gitbub.com/bnetlib/license.html    MIT License
+ */
+
+namespace bnetlib\Resource\Wow\Battlegroups;
+
+use bnetlib\Resource\ConsumeInterface;
+use bnetlib\Resource\ResourceInterface;
+
+/**
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage WorldOfWarcraft
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    http://coss.gitbub.com/bnetlib/license.html    MIT License
+ */
+class Battlegroup implements ResourceInterface, ConsumeInterface
+{
+    /**
+     * @var array
+     */
+    protected $data = array();
+
+    /**
+     * @var \stdClass|null
+     */
+    protected $headers;
+
+    /**
+     * @inheritdoc
+     */
+    public function populate(array $data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getResponseHeaders()
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setResponseHeaders(\stdClass $headers)
+    {
+        $this->headers = $headers;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function consume()
+    {
+        return array('bgslug' => $this->data['slug']);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName()
+    {
+        return $this->data['name'];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getSlug()
+    {
+        return $this->data['slug'];
+    }
+}
---
+++ tests/bnetlibTest/Resource/Wow/BattlegroupsTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage UnitTests
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    http://coss.gitbub.com/bnetlib/license.html    MIT License
+ */
+
+namespace bnetlibTest\Resource\Wow;
+
+use bnetlib\Resource\Wow\Battlegroups;
+
+/**
+ * @category   bnetlib
+ * @package    Resource
+ * @subpackage UnitTests
+ * @group      WorldOFWarcraft
+ * @group      WoW_Battlegroups
+ * @copyright  2012 Eric Boh <cossish@gmail.com>
+ * @license    http://coss.gitbub.com/bnetlib/license.html    MIT License
+ */
+class BattlegroupsTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var bnetlib\Resource\Wow\Battlegroups
+     */
+    protected static $obj;
+
+    public static function setUpBeforeClass()
+    {
+        $data = json_decode(file_get_contents(
+            __DIR__ . DIRECTORY_SEPARATOR . '_files' . DIRECTORY_SEPARATOR . 'battlegroups.json'
+        ), true);
+
+        self::$obj = new Battlegroups();
+        self::$obj->populate($data);
+    }
+
+    public function testIterator()
+    {
+        foreach (self::$obj as $key => $bg) {
+            $this->assertInstanceOf('bnetlib\Resource\Wow\Battlegroups\Battlegroup', $bg);
+        }
+    }
+}
