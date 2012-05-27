6d5ce8358f2a92cb94155276c2e829ff3c7755bb
Eric Boh <cossish@gmail.com>
2012-05-28 01:55:22 +0200

    Added new exceptions

---
+++ src/bnetlib/Exception/ServiceNotCreatedException.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of the bnetlib Library.
+ * Copyright (c) 2012 Eric Boh <cossish@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code. You can also view the
+ * LICENSE file online at https://gitbub.com/coss/bnetlib/LISENCE
+ *
+ * @category  bnetlib
+ * @package   Exception
+ * @copyright 2012 Eric Boh <cossish@gmail.com>
+ * @license   http://coss.gitbub.com/bnetlib/license.html    MIT License
+ */
+
+namespace bnetlib\Exception;
+
+/**
+ * @category  bnetlib
+ * @package   Exception
+ * @copyright 2012 Eric Boh <cossish@gmail.com>
+ * @license   http://coss.gitbub.com/bnetlib/license.html    MIT License
+ */
+class ServiceNotCreatedException extends \Exception implements ExceptionInterface
+{
+
+}
